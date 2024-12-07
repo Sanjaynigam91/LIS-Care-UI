6e28d832ff3153600977e1224ef455e673a2361f
Sanjaynigam91 <nigamsanjay2022@gmail.com>
2024-12-07 18:38:29 +0530

    Code changes for Binding Dropdowns on Test Master Edit
    Code changes for Binding Dropdowns on Test Master Edit

--- HealthCare/src/Pages/Master/Services/testmaster/labtestedit/labtestedit.component.ts
+++ HealthCare/src/Pages/Master/Services/testmaster/labtestedit/labtestedit.component.ts
@@ -9,6 +9,8 @@ import { RefreshPageService } from '../../../../../auth/Shared/refresh-page.serv
 import { Observable } from 'rxjs';
 import { MatIconModule } from '@angular/material/icon';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MetadataService } from '../../../../../auth/metadata.service';
+import { metaTagResponse } from '../../../../../Interfaces/metaTagResponse';
 
 @Component({
   selector: 'app-labtestedit',
@@ -27,34 +29,41 @@ export class LabtesteditComponent {
   loading$!: Observable<boolean>;
   partnerId: string |any;
   labtestCode:string |any;
+  testDepartmentResponse: Observable<metaTagResponse>| any;
+  testSubDepartmentResponse: Observable<metaTagResponse>| any;
+  specimenTypeResponse: Observable<metaTagResponse>| any;
+  reportingStyleResponse: Observable<metaTagResponse>| any;
+  reportTemplatesResponse: Observable<metaTagResponse>| any;
+  metaDataService: any;
 
   constructor(public dialogRef: MatDialogRef<LabtesteditComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,private testService:TestService
-  , private toasterService: ToastService,private refPageService:RefreshPageService){
+  , private toasterService: ToastService,private refPageService:RefreshPageService,private metaService:MetadataService,){
       this.partnerId= localStorage.getItem('partnerId');
       this.labtestCode=data.labtestCode;
     }
 
-
     ngOnInit():void{
       debugger;
-      // this.editPageId= this.lisPageId;
       this.isSubmitVisible=true;
-      // this.getAllCriteria();
-      // this.getAllPageEnities(this.partnerId);
       if(this.labtestCode!==undefined){
         this.isSubmitVisible=false;
         this.isUpdateVisible=true;
         this.isAddHeaderVisible=false;
         this.isEditHeaderVisible=true;
-       // this.GetPageDetailsById(this.lisPageId,this.partnerId);
       }
       else{
+        this.BindAllDepartment();
+        this.BindAllSubDepartment();
+        this.BindTestSpecimenTypes();
+        this.BindAllReportingStyle();
+        this.BindAllReportTemplates();
         this.isSubmitVisible=true;
         this.isUpdateVisible=false;
         this.isAddHeaderVisible=true;
         this.isEditHeaderVisible=false;
       }
+      
       
     }  
 
@@ -65,5 +74,50 @@ export class LabtesteditComponent {
   close(): void {
     this.dialogRef.close();
   }
+
+  BindAllDepartment(){
+    debugger;
+    this.metaService.LoadAllDepartments(this.partnerId).subscribe((response:any)=>{
+      debugger;
+     this.testDepartmentResponse = response.data; 
+     console.log(response);
+    }) 
+   }
+  
+   BindAllSubDepartment(){
+    debugger;
+    this.metaService.LoadSubDepartments(this.partnerId).subscribe((response:any)=>{
+      debugger;
+     this.testSubDepartmentResponse = response.data; 
+     console.log(response);
+    }) 
+   }
+
+   BindTestSpecimenTypes(){
+    debugger;
+    this.metaService.LoadSpecimenType(this.partnerId).subscribe((response:any)=>{
+      debugger;
+     this.specimenTypeResponse = response.data; 
+     console.log(response);
+    }) 
+   }
+
+   BindAllReportingStyle(){
+    debugger;
+    this.metaService.LoadReportingStyle(this.partnerId).subscribe((response:any)=>{
+      debugger;
+     this.reportingStyleResponse = response.data; 
+     console.log(response);
+    }) 
+   }
+
+   BindAllReportTemplates(){
+    debugger;
+    this.metaService.LoadGetReportTemplates(this.partnerId).subscribe((response:any)=>{
+      debugger;
+     this.reportTemplatesResponse = response.data; 
+     console.log(response);
+    }) 
+   }
 
 }
