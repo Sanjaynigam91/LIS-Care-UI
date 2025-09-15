f2f697dc28600b360629ebc25cc65cb90c4b1e8c
Sanjaynigam91 <nigamsanjay2022@gmail.com>
2025-09-15 10:31:26 +0530

    Profile master Code changes
    Profile master Code changes

---
+++ HealthCare/src/Interfaces/ProfileMaster/ProfileResponse.ts
@@ -0,0 +1,33 @@
+// Root response interface
+export interface ProfileResponse {
+  statusCode: number;
+  status: boolean;
+  responseMessage: string;
+  data: Profile[];
+}
+
+// Profile interface
+export interface Profile {
+  partnerId: string;
+  profileCode: string;
+  profileName: string;
+  mrp: number;
+  profileStatus: boolean;
+  b2CRates: number;
+  sampleTypes: string;
+  labrates: number;
+  tatHrs: number;
+  cptCodes: string;
+  printSequence: number;
+  isRestricted: boolean;
+  subProfilesCount: number;
+  recordId: number;
+  normalRangeFooter: string;
+  testShortName: string;
+  profileProfitRate: number;
+  labTestCodes: string;
+  isProfileOutLab: boolean;
+  testApplicable: string;
+  isLMP: boolean;
+  isNABLApplicable: boolean;
+}
--- HealthCare/src/Pages/Master/Services/profilemaster/profilemaster.component.ts
+++ HealthCare/src/Pages/Master/Services/profilemaster/profilemaster.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { LoaderComponent } from '../../../loader/loader.component';
+import { ConfirmationDialogComponentComponent } from '../../../confirmation-dialog-component/confirmation-dialog-component.component';
+import { ToastComponent } from '../../../Toaster/toast/toast.component';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profilemaster',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatPaginatorModule, CommonModule, MatCardModule,
+      MatListModule, MatIconModule, MatButtonModule, NgxDatatableModule, MatSortModule,
+      MatFormFieldModule, MatInputModule, MatSort, NgxPaginationModule,
+      ReactiveFormsModule, LoaderComponent, ConfirmationDialogComponentComponent, ToastComponent],
   templateUrl: './profilemaster.component.html',
   styleUrl: './profilemaster.component.css'
 })
 export class ProfilemasterComponent {
+[x: string]: any;
+  router  =  inject(Router);
 
+  loading$!: Observable<boolean>;
+  partnerId: string |any;
+  loggedInUserId: string |any;
+  p: number = 1; // current page
+  totalItems: number =0; // total number of items, for example
+  itemsPerPage: number = 10; // items per page
+  IsNoRecordFound=false;
+  IsRecordFound=false;
+  sortColumn = '';
+  sortDirection = 'asc';
+  // Filter criteria
+  filterTest: string = '';
+  searchTestForm!: FormGroup;
+  globalRoleForm!:FormGroup
+
+  filteredData: any[] = []; // Data array for the table
+
+   profileMasterForm!: FormGroup;
+
+
+  
 }
