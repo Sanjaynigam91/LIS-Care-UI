2a9be1b77142a2084a46472ff79de048ab79e2b5
Sanjaynigam91 <nigamsanjay2022@gmail.com>
2025-10-17 06:34:41 +0530

    enviornment variable changes
    enviornment variable changes

--- HealthCare/src/app/environments/environments.ts
+++ HealthCare/src/app/environments/environments.ts
@@ -2,6 +2,8 @@
 
 export const environment={
     production:false,
-    apiUrl:"http://localhost:63035/api" // proxy url in developement mode
+   //apiUrl:"http://localhost:63035/api" // proxy url in developement mode
+    apiUrl: 'https://dev-lis-care-api-e8apcmerfne2e0hp.centralindia-01.azurewebsites.net/api'
+
     
 }
