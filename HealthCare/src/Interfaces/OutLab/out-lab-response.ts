b08ed173712a7d245dd7a394c5371ec4d7882f3b
Sanjaynigam91 <nigamsanjay2022@gmail.com>
2025-10-25 10:57:45 +0530

    Out lab Master Code changes
    item.labCode

---
+++ HealthCare/src/Interfaces/OutLab/out-lab-response.ts
@@ -0,0 +1,10 @@
+export interface OutLabResponse {
+  labCode: string;
+  labName: string;
+  city: string;
+  mobileNumber: string;
+  email: string;
+  contactPerson: string;
+  labStatus: boolean;
+  partnerId: string;
+}
