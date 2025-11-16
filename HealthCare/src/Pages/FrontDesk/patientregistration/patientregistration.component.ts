5c29e0d6b921784ef831358f7cca3b8e86451e09
Sanjaynigam91 <nigamsanjay2022@gmail.com>
2025-11-16 09:01:43 +0530

    Patient Registration code Changes
    Patient Registration code Changes

--- HealthCare/src/Pages/FrontDesk/patientregistration/patientregistration.component.ts
+++ HealthCare/src/Pages/FrontDesk/patientregistration/patientregistration.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIcon, MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: 'app-patientregistration',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],     // <-- REQUIRED
   templateUrl: './patientregistration.component.html',
   styleUrl: './patientregistration.component.css'
 })
